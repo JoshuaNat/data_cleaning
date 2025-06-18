4313f15f434fd934d35022059af368904e98a0bb
Joshua <xyznathaniel@gmail.com>
2025-06-17 21:38:42 -0600

    Created staged table

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,1 +1,12 @@
 -- Data cleaning
+
+SELECT * 
+FROM world_layoffs.layoffs;
+
+-- 1 Remove duplicates
+-- 2 Standardize the data
+-- 3 Null values or blank values
+-- 4 Remove Any columns
+
+CREATE TABLE world_layoffs.layoffs_staging
+LIKE layoffs;
