35d4294f455bf019630b925ae8e74df2808eb8de
Joshua <xyznathaniel@gmail.com>
2025-06-17 21:56:57 -0600

    Created CTE to find duplicate tables

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -10,3 +10,25 @@ FROM world_layoffs.layoffs;
 
 CREATE TABLE world_layoffs.layoffs_staging
 LIKE layoffs;
+
+INSERT world_layoffs.layoffs_staging
+SELECT * 
+FROM layoffs;
+
+SELECT * 
+FROM world_layoffs.layoffs_staging;
+
+-- Removing Duplicates / There's not an ID column to make things easy
+
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, 
+industry, total_laid_off, percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM world_layoffs.layoffs_staging
+)
+SELECT * 
+FROM duplicate_cte 
+WHERE row_num > 1;
