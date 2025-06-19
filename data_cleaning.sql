c9b77542cb433f75a679a20fb2653ac155c35226
Joshua <xyznathaniel@gmail.com>
2025-06-18 18:43:22 -0600

    Add query to delete duplicate rows

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -19,6 +19,7 @@ SELECT *
 FROM world_layoffs.layoffs_staging;
 
 -- Removing Duplicates / There's not an ID column to make things easy
+-- First, use ROW_NUMBER to identifie rows with the exact same content
 
 WITH duplicate_cte AS
 (
@@ -31,4 +32,37 @@ FROM world_layoffs.layoffs_staging
 )
 SELECT * 
 FROM duplicate_cte 
+WHERE row_num > 1;
+
+-- MYSQL doesn't allow to DELETE directly from a CTE
+-- Create another staged table, this time including the row_number column, to delete duplicate rows
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, 
+industry, total_laid_off, percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM world_layoffs.layoffs_staging;
+
+DELETE 
+FROM world_layoffs.layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
 WHERE row_num > 1;
